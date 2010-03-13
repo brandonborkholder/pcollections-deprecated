f467a74c0dc4028b12629049a8dfb728e8f4a58c
Brandon Borkholder <borkhobs@gmail.com>
2010-03-12 22:13:52 -0500

    A little more documentation.

--- src/java/pcollections/PersistentCollection.java
+++ src/java/pcollections/PersistentCollection.java
@@ -10,12 +10,19 @@ package pcollections;
  * software.
  */
 /**
- * The root interface in the <i>persistent collection hierarchy</i>. A
- * persistent collection is defined to be a collection of objects, where each
+ * The root interface in the <i>persistent collection hierarchy</i>. The
+ * persistent collections hierarchy reflects the hierarchy of
+ * {@link java.util.Collection}s and intends to follow Java idioms where
+ * appropriate. {@code with(Object)} is analogous to {@code
+ * Collection.add(Object)}, {@code without(Object)} is analogous to {@code
+ * Collection.remove(Object)}.
+ * <p>
+ * A persistent collection is defined to be a collection of objects, where each
  * version of the collection may be accessed and be further manipulated. This is
  * generally termed "fully persistent". This does not necessarily mean that each
  * collection is immutable and thread-safe.
- *
+ * </p>
+ * 
  * @author Brandon Borkholder
  * @version March 3, 2010
  */
@@ -26,7 +33,7 @@ public interface PersistentCollection<T> extends Iterable<T> {
    * collection may not be different than this collection (as in the case of a
    * set), but the new collection is guaranteed to include the value at least
    * once.
-   *
+   * 
    * @param value
    *          the value to include in the collection
    * @return a collection that contains the given value
@@ -39,7 +46,7 @@ public interface PersistentCollection<T> extends Iterable<T> {
    * {@link #with(Object)}, the returned collection is only guaranteed to
    * contain the provided values, not necessarily adding them if they are
    * present already.
-   *
+   * 
    * @param values
    *          an {@code Iterable} representing all the values to add
    * @return a collection that contains all the given values
@@ -50,7 +57,7 @@ public interface PersistentCollection<T> extends Iterable<T> {
    * Returns a collection that does not contain the first occurrence of {@code
    * value} in this collection. If this collection does not contain the given
    * value, the returned collection may be {@code this}.
-   *
+   * 
    * @param value
    *          the value to remove
    * @return a collection that does not contain the given value in the first
@@ -62,7 +69,7 @@ public interface PersistentCollection<T> extends Iterable<T> {
    * Returns a collection that does not contain any of the values in the given
    * {@code Iterable}. If this collection does not intersect the {@code
    * Iterable}, the return collection may be {@code this}.
-   *
+   * 
    * @param values
    *          the values to remove
    * @return a collection that does not contain any of the values from {@code
@@ -73,7 +80,7 @@ public interface PersistentCollection<T> extends Iterable<T> {
   /**
    * Returns {@code true} the provided {@code value} is contained in this
    * collection.
-   *
+   * 
    * @param value
    *          the element to check
    * @return {@code true} if the element is contained in this collection,
@@ -84,7 +91,7 @@ public interface PersistentCollection<T> extends Iterable<T> {
   /**
    * Returns {@code true} if and only if all of the values returned by the
    * {@code Iterable} {@code values} are contained in this collection.
-   *
+   * 
    * @param values
    *          the {@code Iterable} of values to check
    * @return {@code true} if every element is contained in this set, {@code
@@ -95,7 +102,7 @@ public interface PersistentCollection<T> extends Iterable<T> {
   /**
    * Returns the number of values in this collection. If this number is more
    * than {@link Integer#MAX_VALUE}, returns {@code Integer.MAX_VALUE}.
-   *
+   * 
    * @return number of elements in the collection
    */
   int size();
@@ -103,8 +110,38 @@ public interface PersistentCollection<T> extends Iterable<T> {
   /**
    * Returns {@code true} if and only if this collection contains no elements.
    * This is equivalent to the test {@code size() == 0}.
-   *
+   * 
    * @return {@code true} if the collection is empty, {@code false} otherwise
    */
   boolean isEmpty();
+
+  /**
+   * Returns {@code true} if the given object is equal to this {@code
+   * PersistentCollection}. As with {@link java.util.Collection#equals(Object)},
+   * this interface does not add stipulations to the general {@code
+   * Object.equals(Object)}, but implementations are encouraged to override this
+   * method and compare equality based on elements of the collection.
+   * <p>
+   * Note that a {@code PersistentCollection} is never equal to a {@code
+   * java.util.Collection}.
+   * </p>
+   * 
+   * @param obj
+   *          the object to check for equality
+   * @return {@code true} if the given object is equal to this
+   */
+  @Override
+  boolean equals(Object obj);
+
+  /**
+   * Returns the hash code value for this object. As with
+   * {@link java.util.Collection#hashCode()}, this interface does not add
+   * stipulations to the general {@code Object.hashCode()}, but implementations
+   * are encouraged to return a hash code based on the content of the collection
+   * and also appropriately override {@code equals(Object)}.
+   * 
+   * @return the hash code for this collection
+   */
+  @Override
+  int hashCode();
 }
--- src/java/pcollections/PersistentMap.java
+++ src/java/pcollections/PersistentMap.java
@@ -11,6 +11,20 @@ import java.util.Map.Entry;
  * this license. You must not remove this notice, or any other, from this
  * software.
  */
+/**
+ * An object that maps keys to values. A map cannot contain duplicate keys; each
+ * key can map to at most one value.
+ * <p>
+ * A {@code PersistentMap} is mostly equivalent to a {@link java.util.Map} in
+ * that {@code with(Object, Object)} is analogous to {@code Map.put(Object,
+ * Object)}, {@code without(Object)} is analogous to {@code Map.remove(Object)},
+ * and {@code PersistentMap} provides access to the set of keys via {@code
+ * keySet()} and collection of values via {@code values()}.
+ * </p>
+ *
+ * @author Brandon Borkholder
+ * @version March 12, 2010
+ */
 public interface PersistentMap<K, V> extends Iterable<Entry<K, V>> {
   PersistentMap<K, V> with(K key, V value);
 
@@ -33,4 +47,33 @@ public interface PersistentMap<K, V> extends Iterable<Entry<K, V>> {
   int size();
 
   boolean isEmpty();
+
+  /**
+   * Returns {@code true} if the given object is also a {@code PersistentMap}
+   * and has equal content with this {@code PersistentMap}. Two {@code
+   * PersistentMap}s are equal if their sets of keys are equal and for each key,
+   * the two maps produce values which are equal to each other.
+   *
+   * <p>
+   * Note that a {@code PersistentMap} is never equal to a {@code java.util.Map}
+   * .
+   * </p>
+   *
+   * @param obj
+   *          the object to check for equality
+   * @return {@code true} if the given object is equal to this
+   */
+  @Override
+  boolean equals(Object obj);
+
+  /**
+   * Returns the hash code value for this object. The hash code for a {@code
+   * PersistentMap} is defined to be the sum of the hash codes of each pair of
+   * key and value. The hash code for a key/value pair is the hash code of the
+   * key xor'ed with the hash code of the value.
+   *
+   * @return the hash code for this map
+   */
+  @Override
+  int hashCode();
 }
